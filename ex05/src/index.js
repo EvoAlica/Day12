f0691780464384be87f0d5ebaa57c81c71180bab
Tarik <79983026+EvoAlica@users.noreply.github.com>
2021-03-21 19:35:17 +0100

    Update index.js

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,23 +1,22 @@
-// Only change code below this line
-myPetsArray = [
-    {
-      animalType: ["Dog", "Cat", "Bird"],
-      name: ["Pujdo", "Maca", "Tweety"],
-    },
-    {
-      animalType: ["Dog", "Cat", "Bird"],
-      name: ["Pujdo", "Maca", "Tweety"],
-    },
-    {
-      animalType: ["Dog", "Cat", "Bird"],
-      name: ["Pujdo", "Maca", "Tweety"],
-    },
-  ];
-  function myPetsFunction(pets) {
-    return pets[1].name[1];
-  }
-  // Only change code above this line
-  
-  console.log(myPetsFunction(myPetsArray));
-  module.exports = myPetsFunction;
-  module.exports.myPets = myPetsArray;
+var myPetsArray = [
+  {
+    animalType: "Dog",
+    name: "Pujdo",
+  },
+  {
+    animalType: "Cat",
+    name: "Maca",
+  },
+  {
+    animalType: "Bird",
+    name: "Tweety",
+  },
+];
+
+function myPetsFunction(pets) {
+  return pets[1].name;
+}
+
+console.log(myPetsFunction(myPetsArray));
+module.exports = myPetsFunction;
+module.exports.myPets = myPetsArray;
